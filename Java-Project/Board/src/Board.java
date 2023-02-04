b42c451ad491db8216b4aa719a18806d4026394d
anxi01 <tjdals2893@naver.com>
2023-02-05 01:25:28 +0900

    [study] Sign up & Login 구현

--- Java-Project/Board/src/Board.java
+++ Java-Project/Board/src/Board.java
@@ -1,48 +1,90 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+
+/**  문제점
+ *   1. 로그인할 때 아이디가 다르더라도, list가 누적됨
+ *   => 이전 아이디와 현재 입력된 아이디가 다를 경우, 리스트를 초기화 하는 방법(orders.clear())
+ *   But> 이전 아이디와 현재 입력된 아이디를 비교할 수 있는 방법을 찾지 못함.
+ *
+ *   2. SignUp Class를 사용하지 않음.
+ */
+
 
 public class Board {
 
     // private => list 추가 삭제 가능
-    private static ArrayList<Order> orders = new ArrayList<>();
+    private static List<Order> orders = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
-    
+    private static Map<String, String> signUpMap = new HashMap<>();
+
     public static void main(String[] args) {
 
-        boolean loop = true;
+        boolean loop1 = true;
 
-        while(loop) {
-            System.out.println("1. 상품 조회 \t 2. 상품 입력 \t 3. 상품 삭제 \t 4. 상품 검색 \t 5. 나가기");
-            int input = scanner.nextInt();
+        while(loop1) {
 
-            /** 1. 게시판 조회 */
-            if (input == 1) {
-                System.out.println("상품------가격");
-                System.out.println("=============");
-                //System.out.print(orders.get(0).getProduct() + "-----" + orders.get(0).getPrice());
-                for (Order order : orders) {
-                    System.out.println(order.getProduct() + "------" + order.getPrice());
-                }
+            System.out.println("1. 회원가입 \t 2. 로그인 \t 3. 로그아웃");
+            int input1 = scanner.nextInt();
+
+            if (input1 == 1){
+                System.out.println("아이디를 입력해주세요.");
+                String id = scanner.next();
+                System.out.println("비밀번호를 입력해주세요.");
+                String password = scanner.next();
+
+                signUpMap.put(id, password);
             }
-            /** 2. 게시판 입력 */
-            if (input == 2) {
-                System.out.println("상품을 추가해주세요.");
-                String product = scanner.next();
-                System.out.println("가격을 추가해주세요.");
-                String price = scanner.next();
 
-                orders.add(new Order(product, price));
-            }
-            /** 3. 게시판 삭제 */
-            if (input == 3) {
-                System.out.println("제거할 상품명 혹은 상품가격을 입력해주세요.");
-                String deleteProduct = scanner.next();
-                for (int i = 0; i < orders.size(); i++) {
-                    if(orders.get(i).getProduct().equals(deleteProduct) || orders.get(i).getPrice().equals(deleteProduct)){
-                        orders.remove(i);
+            /** 로그인 */
+            if (input1 == 2) {
+                System.out.println("아이디를 입력해주세요.");
+                String id = scanner.next();
+                System.out.println("비밀번호를 입력해주세요.");
+                String password = scanner.next();
+
+                boolean loop2;
+
+                if(signUpMap.containsKey(id) && signUpMap.get(id).equals(password)){
+                    loop2 = true;
+                } else{loop2 = false;}
+
+                while (loop2) {
+
+                    System.out.println("1. 상품 조회 \t 2. 상품 입력 \t 3. 상품 삭제 \t 4. 상품 검색 \t 5. 나가기");
+                    int input2 = scanner.nextInt();
+
+                    /** 1. 게시판 조회 */
+                    if (input2 == 1) {
+                        System.out.println("상품------가격");
+                        System.out.println("=============");
+                        //System.out.print(orders.get(0).getProduct() + "-----" + orders.get(0).getPrice());
+                        for (Order order : orders) {
+                            System.out.println(order.getProduct() + "------" + order.getPrice());
+                        }
                     }
-                }
-            }
+                    /** 2. 게시판 입력 */
+                    if (input2 == 2) {
+                        System.out.println("상품을 추가해주세요.");
+                        String product = scanner.next();
+                        System.out.println("가격을 추가해주세요.");
+                        String price = scanner.next();
+
+                        orders.add(new Order(product, price));
+                    }
+                    /** 3. 게시판 삭제 */
+                    if (input2 == 3) {
+                        System.out.println("제거할 상품명 혹은 상품가격을 입력해주세요.");
+                        String deleteProduct = scanner.next();
+                        for (int i = 0; i < orders.size(); i++) {
+                            if (orders.get(i).getProduct().equals(deleteProduct) || orders.get(i).getPrice().equals(deleteProduct)) {
+                                orders.remove(i);
+                            }
+                        }
+//                for(Order order : orders){
+//                    if(order.getProduct().equals(deleteProduct) || order.getPrice().equals(deleteProduct)){
+//                        orders.remove(order.getProduct());
+//                    }
+//                }
+                    }
 //                ----------------------------------------------------------------------------------
 //                System.out.println("가격을 추가해주세요.");
 //                String price = scanner.next();
@@ -55,23 +97,34 @@ public class Board {
 //                if(productInfo.equals(orders)){
 //                    orders.remove(productInfo);
 //                }
-            /** 4. 상품 검색 */
-            if (input == 4){
-                System.out.println("검색할 상품명 혹은 상품가격을 입력하세요.");
-                String searchProduct = scanner.next();
+                    /** 4. 상품 검색 */
+                    if (input2 == 4) {
+                        System.out.println("검색할 상품명 혹은 상품가격을 입력하세요.");
+                        String searchProduct = scanner.next();
 
-                for(int i = 0; i < orders.size(); i++){
-                    if(orders.get(i).getProduct().equals(searchProduct) || orders.get(i).getPrice().equals(searchProduct)){
-                        System.out.println("상품 ------ 가격");
-                        System.out.println(orders.get(i).getProduct() + "------" + orders.get(i).getPrice());
+                        for (int i = 0; i < orders.size(); i++) {
+                            if (orders.get(i).getProduct().equals(searchProduct) || orders.get(i).getPrice().equals(searchProduct)) {
+                                System.out.println("상품 ------ 가격");
+                                System.out.println(orders.get(i).getProduct() + "------" + orders.get(i).getPrice());
+                            }
+                        }
+//                for(Order order : orders){
+//                    if(order.getProduct().equals(searchProduct) || order.getPrice().equals(searchProduct)){
+//                        System.out.println("상품 ------ 가격");
+//                        System.out.println(order.getProduct() + "------" + order.getPrice());
+//                    }
+//                }
+                    }
+                    /** 5. 나가기 */
+                    if (input2 == 5) {
+                        loop2 = false;
                     }
                 }
             }
-
-            /** 5. 나가기 */
-            if (input == 5){
-                loop = false;
-            }
+                /** 3. 로그아웃 */
+                if(input1 == 3){
+                    loop1 = false;
+                }
         }
     }
 }
---
+++ Java-Project/Board/src/SignUp.java
@@ -0,0 +1,13 @@
+public class SignUp {
+
+    String id;
+    String password;
+
+    public SignUp(String id, String password){
+        this.id = id;
+        this.password = password;
+    }
+    public SignUp(){}
+
+
+}
